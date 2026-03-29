65a325864838cea667f97aea3722ae4cef969bec
UCJung <mykumi@gmail.com>
2026-03-29 01:53:29 +0000

    Deploying to gh-pages from @ UCJung/ucjung.github.io@25b237f2564c4b2627ddc2ceafb8c19452884924 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -19,9 +19,9 @@ function determineGiscusTheme() {
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
     "data-repo": "UCJung/ucjung.github.io",
-    "data-repo-id": "",
+    "data-repo-id": "R_kgDORzIweA",
     "data-category": "Announcements",
-    "data-category-id": "",
+    "data-category-id": "DIC_kwDORzIweM4C5grs",
     "data-mapping": "title",
     "data-strict": "1",
     "data-reactions-enabled": "1",
